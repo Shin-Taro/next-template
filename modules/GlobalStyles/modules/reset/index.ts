b8a1985aede47b298b09b7b5ddc31a74740dc646
ShintaroTakahashi <shintaro_takahashi@bita.jp>
2022-08-21 16:08:55 +0900

    fix: add outline styles on reset css

--- modules/GlobalStyles/modules/reset/index.ts
+++ modules/GlobalStyles/modules/reset/index.ts
@@ -3,15 +3,29 @@ import ress from "ress";
 
 export const reset = css`
   ${ress}
+
   * {
     box-sizing: border-box;
     font-family: inherit;
     font-size: inherit;
     font-weight: inherit;
   }
+
   li {
     list-style: none;
   }
+
+  input,
+  select,
+  textarea,
+  button,
+  object,
+  a[href] {
+    &:focus {
+      outline: solid 1px black;
+    }
+  }
+
   html {
     font-size: 62.5%;
   }
